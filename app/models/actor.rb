1dcab48cd69be75fa537c866ed8ce93fbcf613df
github username <github email address>
2020-01-27 11:15:26 -0800

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+
+    def full_name
+        # binding.pry
+        "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+
+
+
+
 end
