d01b89f2ea11949d57128647fe53a2f0f211efca
Chris Morgan <cwm111794@gmail.com>
2020-01-28 16:27:45 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    return "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    characters_played = self.characters.map {|character| character.name}
+    shows_in = self.shows.map {|show| show.name}
+    role = characters_played.concat shows_in
+    roles << role.join(" - ")
+    roles
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def actors_list 
+    actors = []
+    firstname = self.actors.collect {|actor| actor.first_name }
+    lastname = self.actors.collect {|actor| actor.last_name }
+    allactors = firstname.concat lastname
+    actors << allactors.join(" ")
+    actors
+  end
 end
