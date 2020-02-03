75d67232445c64d4d5cd418a6602e252bc5eb433
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-02-03 23:08:07 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+  belongs_to :genre 
+  
+  def actors_list 
+    shows.actors do 
+      actors.full_name.collect[]
+    end 
+  end 
+  
   
 end
